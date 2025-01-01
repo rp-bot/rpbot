e5e10361c46fbe2b74f4698a6be519b282a6d69f
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2024-12-31 23:05:39 -0500

    published it

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="rohit_prat_bot",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Rohit Prat",
+    author_email="legacy.business18@gmail.com",
     description="A short description of your package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_package",
+    url="https://github.com/rp-bot/rpbot",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
