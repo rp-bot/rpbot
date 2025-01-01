41c03509d73c37d518c01f0fc964b8b16080d5bb
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2024-12-31 22:54:59 -0500

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rohit Prat Bot (rpbot)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rohit_prat_bot",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A short description of your package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add your package dependencies here
+    ],
+)
